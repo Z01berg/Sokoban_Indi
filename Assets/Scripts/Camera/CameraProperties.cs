00cc992e10ba627c5b77680f3f8fa9d8f1f44bf3
Z01berg <zaharzubik@gmail.com>
2024-05-30 21:46:36 +0200

    Fix: Camera bug

--- Assets/Scripts/Camera/CameraProperties.cs
+++ Assets/Scripts/Camera/CameraProperties.cs
@@ -8,24 +8,27 @@ public class CameraProperties : MonoBehaviour
 {
     private CinemachineVirtualCamera _virtualCamera;
     private float _zoomSpeed = 2f;
-    private float _minZoom = 0f;
+    private float _minZoom = 1f;
     private float _maxZoom = 20f;
+    private float _currentZoom;
+    private float _newZoom;
+    private float _zoomInput;
 
     public void Awake()
     {
         _virtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
 
-    void Update()
+    void Update()//TODO: Optimaze
     {
-        float zoomInput = Input.GetAxis("Mouse ScrollWheel");
-        ZoomCamera(zoomInput);
+        _zoomInput = Input.GetAxis("Mouse ScrollWheel");
+        ZoomCamera(_zoomInput);
     }
 
     void ZoomCamera(float increment)
     {
-        float currentZoom = _virtualCamera.m_Lens.OrthographicSize;
-        float newZoom = Mathf.Clamp(currentZoom - increment * _zoomSpeed, _minZoom, _maxZoom);
-        _virtualCamera.m_Lens.OrthographicSize = newZoom;
+        _currentZoom = _virtualCamera.m_Lens.OrthographicSize;
+        _newZoom = Mathf.Clamp(_currentZoom - increment * _zoomSpeed, _minZoom, _maxZoom);
+        _virtualCamera.m_Lens.OrthographicSize = _newZoom;
     }
 }
